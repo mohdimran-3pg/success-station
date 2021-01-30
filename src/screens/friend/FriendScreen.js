558248f51788e165b8c56ebc7e671fb86200e5d2
Mohd Imran <mohd.imran.3pg@gmail.com>
2021-01-30 18:28:18 +0530

    Added Friend Screen

---
+++ src/screens/friend/FriendScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View,Text, Image, StyleSheet, SafeAreaView, TouchableOpacity, FlatList} from 'react-native'
+import InputView from '../../../components/InputView'
+import DropDownSelectBox from '../../../components/DropDownSelectBox'
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+
+export default class FriendScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+    
+    componentDidMount() {
+    }
+
+    componentWillUnmount() {
+    }
+
+    render() {
+        return (<View></View>)
+    }
+}
