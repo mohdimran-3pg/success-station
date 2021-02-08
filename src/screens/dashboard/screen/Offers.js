8a9367f9a128f0b39415ce9483374f66f73712a9
Mohd Imran <mohd.imran.3pg@gmail.com>
2021-02-08 11:05:15 +0530

    - Offer screen work

--- src/screens/dashboard/screen/Offers.js
+++ src/screens/dashboard/screen/Offers.js
@@ -3,103 +3,123 @@
 import * as React from 'react';
 import {Button, View, Text, SafeAreaView, FlatList, Image, StyleSheet, TouchableOpacity, TouchableWithoutFeedback} from 'react-native';
 import { Searchbar ,DefaultTheme} from 'react-native-paper';
+import Carousel, { Pagination } from 'react-native-snap-carousel'
+
+const SLIDER_WIDTH = Dimensions.get('window').width + 80
+const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7)
+
+const CarouselCardItem = ({ item, index }) => {
+  return (
+    <View style={styles.container} key={index}>
+      <Image
+        source={{ uri: item.imgUrl }}
+        style={styles.image}
+      />
+      <Text style={styles.header}>{item.title}</Text>
+      <Text style={styles.body}>{item.body}</Text>
+    </View>
+  )
+}
+const CarouselCards = () => {
+  const isCarousel = React.useRef(null)
+
+  return (
+    <View>
+      <Carousel
+        layout="tinder"
+        layoutCardOffset={9}
+        ref={isCarousel}
+        data={data}
+        renderItem={CarouselCardItem}
+        sliderWidth={SLIDER_WIDTH}
+        itemWidth={ITEM_WIDTH}
+        inactiveSlideShift={0}
+        useScrollView={true}
+      />
+    </View>
+  )
+}
 
 const UserProfile =(user) => {
   console.log('THis is User:::', user)
   return (
-    <View style={{width:'49%', marginEnd:'2%', marginTop: 10, borderColor: "#00000030", borderWidth: 1, borderRadius: 4}}>
-        <View style={{width: 60, height: 60, borderRadius: 30, alignSelf: "center", marginTop: 21}}>
-            <Image style={{width: 60, height: 60, borderRadius: 30}} 
-                  source={{uri: user.src}}
+    <View style={{width:"32%",height:110, marginTop: 30}}>
+        <View style={{width: "100%", height: 90, borderRadius: 30, marginTop: 21,  borderWidth: 1,
+    borderRadius: 4,
+    borderColor: 'rgba(158, 166, 190, 0.12)',
+    borderBottomWidth: 0,
+    shadowColor: 'rgba(0, 0, 0, 0.06)',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.9,
+    shadowRadius: 4,
+    elevation: 3, backgroundColor:"#FFFFFF", justifyContent: "center"}}>
+            <Image style={{width: 39, height: 33, alignSelf: "center"}} 
+                  source={user.image}
+                  resizeMode="contain"
             />
         </View>
         <View style={{height: 40, marginTop:7}}>
-          <Text style={{fontSize: 15, fontWeight: "700", textAlign: "center", color: "#000000"}}>{user.name}</Text>
-          <Text style={{fontSize: 15, fontWeight: "400", textAlign: "center", color: "#9EA6BE",marginTop:9}}>{user.role}</Text>
-        </View>
-        <View style={{width: "70%", alignSelf: "center",marginTop:15, justifyContent: "space-between", flexDirection: "row", height: 30}}>
-          <Image style={{width: 24, height:24}}
-              source={require('../../../../assets/friends/graduation-icon.png')}
-          />
-          <Text style={{fontSize: 11, fontWeight: "400",marginStart:4, color: "#9EA6BE", fontStyle: "normal"}}>{user.location}</Text>
-        </View>
-        <View style={{width: "80%", alignSelf: "center", height: 35, marginBottom: 10,marginTop:13}}>
-        <View style={styles.mainView}>
-            <TouchableOpacity>
-            <Text style={styles.buttonStyle}>
-            Connect
-            </Text>
-            </TouchableOpacity>
-        </View>
+          <Text style={{fontSize: 16, fontWeight: "500", textAlign: "center", color: "#000000"}}>{user.name}</Text>
         </View>
     </View>
   )
 }
+
+const data = [
+  {
+    title: "Aenean leo",
+    body: "Ut tincidunt tincidunt erat. Sed cursus turpis vitae tortor. Quisque malesuada placerat nisl. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
+    imgUrl: "https://picsum.photos/id/11/200/300"
+  },
+  {
+    title: "In turpis",
+    body: "Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ",
+    imgUrl: "https://picsum.photos/id/10/200/300"
+  },
+  {
+    title: "Lorem Ipsum",
+    body: "Phasellus ullamcorper ipsum rutrum nunc. Nullam quis ante. Etiam ultricies nisi vel augue. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.",
+    imgUrl: "https://picsum.photos/id/12/200/300"
+  }
+]
 
 const friendsData = [
   {
     id: 11,
     src:
       'https://phlearn.com/wp-content/uploads/2019/04/Top-20-Photog-Books-no-text.jpg?fit=1400%2C628&quality=99&strip=all',
-    role: 'Student',
-    name: 'Tabrez Ahmed',
-    location: 'ABES Institute Of Technology',
-  },
-  {
-    id: 12,
+    name: 'Wrote',
+    image: require('../../../../assets/categories/wrote-category.png')
+  },{
+    id: 11,
     src:
       'https://phlearn.com/wp-content/uploads/2019/04/Top-20-Photog-Books-no-text.jpg?fit=1400%2C628&quality=99&strip=all',
-    role: 'Student',
-    name: 'Mohammad Iman',
-    location: 'ABES Institute Of Technology',
-  },
-  {
-    id: 13,
+    name: 'Medical supplies',
+    image: require('../../../../assets/categories/medical.png')
+  },{
+    id: 11,
     src:
       'https://phlearn.com/wp-content/uploads/2019/04/Top-20-Photog-Books-no-text.jpg?fit=1400%2C628&quality=99&strip=all',
-    role: 'Student',
-    name: 'Rahul Pandey',
-    location: 'ABES Institute Of Technology',
-  },
-  {
-    id: 14,
+    name: 'Engineering Supplies',
+    image: require('../../../../assets/categories/engineering.png')
+  },{
+    id: 11,
     src:
       'https://phlearn.com/wp-content/uploads/2019/04/Top-20-Photog-Books-no-text.jpg?fit=1400%2C628&quality=99&strip=all',
-    role: 'Student',
-    name: 'Pathak Ji',
-    location: 'ABES Institute Of Technology',
-  },
-  {
-    id: 15,
+    name: 'Wrote',
+    image: require('../../../../assets/categories/wrote-category.png')
+  },{
+    id: 11,
     src:
       'https://phlearn.com/wp-content/uploads/2019/04/Top-20-Photog-Books-no-text.jpg?fit=1400%2C628&quality=99&strip=all',
-    role: 'Student',
-    name: 'Amanat ali',
-    location: 'ABES Institute Of Technology',
-  },
-  {
-    id: 16,
+    name: 'Medical supplies',
+    image: require('../../../../assets/categories/medical.png')
+  },{
+    id: 11,
     src:
       'https://phlearn.com/wp-content/uploads/2019/04/Top-20-Photog-Books-no-text.jpg?fit=1400%2C628&quality=99&strip=all',
-    role: 'Student',
-    name: 'Amanat ali',
-    location: 'ABES Institute Of Technology',
-  },
-  {
-    id: 17,
-    src:
-      'https://phlearn.com/wp-content/uploads/2019/04/Top-20-Photog-Books-no-text.jpg?fit=1400%2C628&quality=99&strip=all',
-    role: 'Student',
-    name: 'Amanat ali',
-    location: 'ABES Institute Of Technology',
-  },
-  {
-    id: 18,
-    src:
-      'https://phlearn.com/wp-content/uploads/2019/04/Top-20-Photog-Books-no-text.jpg?fit=1400%2C628&quality=99&strip=all',
-    role: 'Student',
-    name: 'Amanat ali',
-    location: 'ABES Institute Of Technology',
+    name: 'Engineering Supplies',
+    image: require('../../../../assets/categories/engineering.png')
   },
 
 ];
@@ -143,16 +163,44 @@ export default class OffersScreen extends React.Component {
             />
             </View>
             <View style={{width: "100%", height: "20%", backgroundColor: "red"}}>
-
+              <Carousel
+                layout="tinder"
+                layoutCardOffset={9}
+                ref={isCarousel}
+                data={data}
+                renderItem={CarouselCardItem}
+                sliderWidth={SLIDER_WIDTH}
+                itemWidth={ITEM_WIDTH}
+                onSnapToItem={(index) => setIndex(index)}
+                useScrollView={true}
+              />
+              <Pagination
+              dotsLength={data.length}
+              activeDotIndex={index}
+              carouselRef={isCarousel}
+              dotStyle={{
+                width: 10,
+                height: 10,
+                borderRadius: 5,
+                marginHorizontal: 0,
+                backgroundColor: 'rgba(0, 0, 0, 0.92)'
+              }}
+              inactiveDotOpacity={0.4}
+              inactiveDotScale={0.6}
+              tappableDots={true}
+            />
             </View>
             <View style={{width: "100%", backgroundColor: "white", height: "70%"}}>
+                <View style={{width: "90%", alignSelf: "center"}}>
+                  <Text style={{fontSize: 20, fontWeight: "700", fontStyle: "normal"}}>Categories</Text>
+                </View>
                 <View style={{width: "90%", alignSelf: "center", height: "100%"}}>
                 <FlatList
                     columnWrapperStyle={{justifyContent: 'space-between'}}
                     style = {{width: "100%"}}
                     keyExtractor = {(item) => item.id} 
                     data = {friendsData}
-                    numColumns={2}
+                    numColumns={3}
                     renderItem={({item}) => UserProfile(item)} 
                 />
                 </View>
@@ -177,5 +225,37 @@ const styles = StyleSheet.create({
       borderColor: "#F78A3A",
       borderWidth: 1
   },
-  buttonStyle: {color: "#F78A3A", fontSize: 17, fontWeight: "700", textAlign: "center"}
+  buttonStyle: {color: "#F78A3A", fontSize: 17, fontWeight: "700", textAlign: "center"},
+  container: {
+    backgroundColor: 'white',
+    borderRadius: 8,
+    width: ITEM_WIDTH,
+    paddingBottom: 40,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
+  },
+  image: {
+    width: ITEM_WIDTH,
+    height: 300,
+  },
+  header: {
+    color: "#222",
+    fontSize: 28,
+    fontWeight: "bold",
+    paddingLeft: 20,
+    paddingTop: 20
+  },
+  body: {
+    color: "#222",
+    fontSize: 18,
+    paddingLeft: 20,
+    paddingLeft: 20,
+    paddingRight: 20
+  }
 });
