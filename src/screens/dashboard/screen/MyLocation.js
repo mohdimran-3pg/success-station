01e24319fd2a05d25fc65246982a82806043684a
rahul.pandey <rahulsaty@gmail.com>
2021-02-27 15:19:30 +0530

    Add google map

--- src/screens/dashboard/screen/MyLocation.js
+++ src/screens/dashboard/screen/MyLocation.js
@@ -1,17 +1,26 @@
 // React Navigate Drawer with Bottom Tab
 // https://aboutreact.com/bottom-tab-view-inside-navigation-drawer/
+import React, { useState } from "react";
+import {Button, View, Text, SafeAreaView,StyleSheet} from 'react-native';
 
-import * as React from 'react';
-import {Button, View, Text, SafeAreaView} from 'react-native';
+import MapView, { Marker } from "react-native-maps";
 
-import WebView from 'react-native-webview'
-const MyLoacationScreen = ({navigation}) => {
+const MyLoacationScreen = (navigation) => {
+  const [region, setRegion] = useState({
+    latitude: 21.487301,
+    longitude: 39.181339,
+    latitudeDelta: 0.009,
+    longitudeDelta: 0.009
+  });
+
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={ {flex:1 ,justifyContent:'center', margin:16}} >
-     <WebView source = {{uri: 'https://www.google.co.in/maps/place/Ghran+Saudi+Arabia/@22.0206093,39.3224688,12z/data=!3m1!4b1!4m13!1m7!3m6!1s0x15c3d01fb1137e59:0xe059579737b118db!2sJeddah+Saudi+Arabia!3b1!8m2!3d21.485811!4d39.1925048!3m4!1s0x15c1a151f20f3aad:0x7c8b0ea0da3b572f!8m2!3d22.0194532!4d39.3825531'}}/>
-        </View>
-    </SafeAreaView>
+    <MapView
+      style={{ flex: 1 }}
+      region={region}
+      onRegionChangeComplete={region => setRegion(region)}
+    >
+      <Marker coordinate={{ latitude: 21.487301, longitude:39.181339 }} />
+    </MapView>
   );
 };
 
